37f62146571379b2ff58ea315bcb8b783ab512ff
John Ayeni <johnayeni10.aj@gmail.com>
2019-04-23 22:23:33 +0100

    🤔

--- src/org/tensorflow/yolo/util/ClassAttrProvider.java
+++ src/org/tensorflow/yolo/util/ClassAttrProvider.java
@@ -11,65 +11,62 @@ import java.io.InputStreamReader;
 import java.util.Vector;
 
 /**
- * It is used to read names of the classes from the specified resource.
- * It also specifies a color for each classes.
- *
- * Created by Zoltan Szabo on 12/17/17.
- * URL: https://github.com/szaza/android-yolo-v2
+ * It is used to read names of the classes from the specified resource. It also
+ * specifies a color for each classes.
  */
 
 public final class ClassAttrProvider {
-    private final Vector<String> labels = new Vector();
-    private final Vector<Integer> colors = new Vector();
-    private static ClassAttrProvider instance;
+  private final Vector<String> labels = new Vector();
+  private final Vector<Integer> colors = new Vector();
+  private static ClassAttrProvider instance;
 
-    private ClassAttrProvider(final AssetManager assetManager) {
-        init(assetManager);
+  private ClassAttrProvider(final AssetManager assetManager) {
+    init(assetManager);
+  }
+
+  public static ClassAttrProvider newInstance(final AssetManager assetManager) {
+    if (instance == null) {
+      instance = new ClassAttrProvider(assetManager);
     }
 
-    public static ClassAttrProvider newInstance(final AssetManager assetManager) {
-        if (instance == null) {
-            instance = new ClassAttrProvider(assetManager);
-        }
+    return instance;
+  }
 
-        return instance;
+  private void init(final AssetManager assetManager) {
+    try (BufferedReader br = new BufferedReader(new InputStreamReader(assetManager.open(Config.LABEL_FILE)))) {
+      String line;
+      while ((line = br.readLine()) != null) {
+        labels.add(line);
+        colors.add(convertClassNameToColor(line));
+      }
+    } catch (IOException ex) {
+      throw new RuntimeException("Problem reading label file!", ex);
+    }
+  }
+
+  private int convertClassNameToColor(String className) {
+    byte[] rgb = new byte[3];
+    byte[] name = className.getBytes();
+
+    for (int i = 0; i < name.length; i++) {
+      rgb[i % 3] += name[i];
     }
 
-    private void init(final AssetManager assetManager) {
-        try (BufferedReader br = new BufferedReader(new InputStreamReader(assetManager.open(Config.LABEL_FILE)))) {
-            String line;
-            while ((line = br.readLine()) != null) {
-                labels.add(line);
-                colors.add(convertClassNameToColor(line));
-            }
-        } catch (IOException ex) {
-            throw new RuntimeException("Problem reading label file!", ex);
-        }
+    // Hue saturation
+    for (int i = 0; i < rgb.length; i++) {
+      if (rgb[i] < 120) {
+        rgb[i] += 120;
+      }
     }
 
-    private int convertClassNameToColor(String className) {
-        byte[] rgb = new byte[3];
-        byte[] name = className.getBytes();
+    return Color.rgb(rgb[0], rgb[1], rgb[2]);
+  }
 
-        for (int i=0; i<name.length; i++) {
-            rgb[i%3] += name[i];
-        }
+  public Vector<String> getLabels() {
+    return labels;
+  }
 
-        // Hue saturation
-        for (int i=0; i<rgb.length; i++) {
-            if (rgb[i] < 120) {
-                rgb[i] += 120;
-            }
-        }
-
-        return Color.rgb(rgb[0], rgb[1], rgb[2]);
-    }
-
-    public Vector<String> getLabels() {
-        return labels;
-    }
-
-    public Vector<Integer> getColors() {
-        return colors;
-    }
+  public Vector<Integer> getColors() {
+    return colors;
+  }
 }
--- src/org/tensorflow/yolo/view/CameraActivity.java
+++ src/org/tensorflow/yolo/view/CameraActivity.java
@@ -18,123 +18,118 @@ import org.tensorflow.yolo.R;
 import static org.tensorflow.yolo.Config.LOGGING_TAG;
 
 /**
- * Camera activity class.
- * Modified by Zoltan Szabo
+ * Camera activity class. Modified by Zoltan Szabo
  */
 public abstract class CameraActivity extends Activity implements OnImageAvailableListener {
-    private static final int PERMISSIONS_REQUEST = 1;
+  private static final int PERMISSIONS_REQUEST = 1;
 
-    private Handler handler;
-    private HandlerThread handlerThread;
+  private Handler handler;
+  private HandlerThread handlerThread;
 
-    @Override
-    protected void onCreate(final Bundle savedInstanceState) {
-        super.onCreate(null);
-        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+  @Override
+  protected void onCreate(final Bundle savedInstanceState) {
+    super.onCreate(null);
+    getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
 
-        setContentView(R.layout.activity_camera);
+    setContentView(R.layout.activity_camera);
 
-        if (hasPermission()) {
-            setFragment();
-        } else {
-            requestPermission();
-        }
+    if (hasPermission()) {
+      setFragment();
+    } else {
+      requestPermission();
+    }
+  }
+
+  @Override
+  public synchronized void onResume() {
+    super.onResume();
+
+    handlerThread = new HandlerThread("inference");
+    handlerThread.start();
+    handler = new Handler(handlerThread.getLooper());
+  }
+
+  @Override
+  public synchronized void onPause() {
+    if (!isFinishing()) {
+      finish();
     }
 
-    @Override
-    public synchronized void onResume() {
-        super.onResume();
-
-        handlerThread = new HandlerThread("inference");
-        handlerThread.start();
-        handler = new Handler(handlerThread.getLooper());
+    handlerThread.quitSafely();
+    try {
+      handlerThread.join();
+      handlerThread = null;
+      handler = null;
+    } catch (final InterruptedException ex) {
+      Log.e(LOGGING_TAG, "Exception: " + ex.getMessage());
     }
 
-    @Override
-    public synchronized void onPause() {
-        if (!isFinishing()) {
-            finish();
-        }
+    super.onPause();
+  }
 
-        handlerThread.quitSafely();
-        try {
-            handlerThread.join();
-            handlerThread = null;
-            handler = null;
-        } catch (final InterruptedException ex) {
-            Log.e(LOGGING_TAG, "Exception: " + ex.getMessage());
-        }
-
-        super.onPause();
+  protected synchronized void runInBackground(final Runnable runnable) {
+    if (handler != null) {
+      handler.post(runnable);
     }
+  }
 
-    protected synchronized void runInBackground(final Runnable runnable) {
-        if (handler != null) {
-            handler.post(runnable);
-        }
+  @Override
+  public void onRequestPermissionsResult(final int requestCode, final String[] permissions, final int[] grantResults) {
+    switch (requestCode) {
+    case PERMISSIONS_REQUEST: {
+      if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED
+          && grantResults[1] == PackageManager.PERMISSION_GRANTED) {
+        setFragment();
+      } else {
+        requestPermission();
+      }
     }
-
-    @Override
-    public void onRequestPermissionsResult(final int requestCode, final String[] permissions,
-                                           final int[] grantResults) {
-        switch (requestCode) {
-            case PERMISSIONS_REQUEST: {
-                if (grantResults.length > 0
-                        && grantResults[0] == PackageManager.PERMISSION_GRANTED
-                        && grantResults[1] == PackageManager.PERMISSION_GRANTED) {
-                    setFragment();
-                } else {
-                    requestPermission();
-                }
-            }
-        }
     }
+  }
 
-    private boolean hasPermission() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            return checkSelfPermission(Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED
-                    && checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED;
-        } else {
-            return true;
-        }
+  private boolean hasPermission() {
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+      return checkSelfPermission(Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED
+          && checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED;
+    } else {
+      return true;
     }
+  }
 
-    private void requestPermission() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            if (shouldShowRequestPermissionRationale(Manifest.permission.CAMERA)
-                    || shouldShowRequestPermissionRationale(Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
-                Toast.makeText(CameraActivity.this,
-                        "Camera AND storage permission are required for this demo", Toast.LENGTH_LONG).show();
-            }
-            requestPermissions(new String[]{Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE}, PERMISSIONS_REQUEST);
-        }
+  private void requestPermission() {
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+      if (shouldShowRequestPermissionRationale(Manifest.permission.CAMERA)
+          || shouldShowRequestPermissionRationale(Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+        Toast.makeText(CameraActivity.this, "Camera AND storage permission are required for this demo",
+            Toast.LENGTH_LONG).show();
+      }
+      requestPermissions(new String[] { Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE },
+          PERMISSIONS_REQUEST);
     }
+  }
 
-    protected void setFragment() {
-        CameraConnectionFragment cameraConnectionFragment = new CameraConnectionFragment();
-        cameraConnectionFragment.addConnectionListener((final Size size, final int rotation) ->
-                CameraActivity.this.onPreviewSizeChosen(size, rotation));
-        cameraConnectionFragment.addImageAvailableListener(this);
+  protected void setFragment() {
+    CameraConnectionFragment cameraConnectionFragment = new CameraConnectionFragment();
+    cameraConnectionFragment.addConnectionListener(
+        (final Size size, final int rotation) -> CameraActivity.this.onPreviewSizeChosen(size, rotation));
+    cameraConnectionFragment.addImageAvailableListener(this);
 
-        getFragmentManager()
-                .beginTransaction()
-                .replace(R.id.container, cameraConnectionFragment)
-                .commit();
+    getFragmentManager().beginTransaction().replace(R.id.container, cameraConnectionFragment).commit();
+  }
+
+  public void requestRender() {
+    final OverlayView overlay = (OverlayView) findViewById(R.id.overlay);
+    if (overlay != null) {
+      overlay.postInvalidate();
     }
+  }
 
-    public void requestRender() {
-        final OverlayView overlay = (OverlayView) findViewById(R.id.overlay);
-        if (overlay != null) {
-            overlay.postInvalidate();
-        }
+  public void addCallback(final OverlayView.DrawCallback callback) {
+    final OverlayView overlay = (OverlayView) findViewById(R.id.overlay);
+    if (overlay != null) {
+      overlay.addCallback(callback);
     }
+  }
 
-    public void addCallback(final OverlayView.DrawCallback callback) {
-        final OverlayView overlay = (OverlayView) findViewById(R.id.overlay);
-        if (overlay != null) {
-            overlay.addCallback(callback);
-        }
-    }
-
-    protected abstract void onPreviewSizeChosen(final Size size, final int rotation);
+  protected abstract void onPreviewSizeChosen(final Size size, final int rotation);
 }
